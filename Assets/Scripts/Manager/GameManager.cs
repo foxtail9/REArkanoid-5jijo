0237e7ec06dd0e3f261ff3c454573eb18d933f20
ekrxjvpvj0110 <ekrxjvpvj0110@gmail.com>
2024-10-16 22:22:01 +0900

    Merge branch 'Dev' into MinJun

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,47 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    [SerializeField] private GameObject _paddle1;
+    [SerializeField] private GameObject _paddle2;
+    [SerializeField] private GameObject _brick;
+    [SerializeField] private GameObject _ball;
+
+    [SerializeField] private Text Life;
+    [SerializeField] private Text Score;
+    //[SerializeField] private GameObject GameOverPanel;
+
+    private int _life = 3;
+    private int _score = 0;
+
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
-            Instance = new GameManager();
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
         Time.timeScale = 1.0f;
     }
 
-    [SerializeField] private GameObject _paddle;
-    [SerializeField] private GameObject _brick;
-    [SerializeField] private GameObject _ball;
-
-    [SerializeField] private GameObject GameOverPanel;
-
     // Start is called before the first frame update
     void Start()
     {
-        // ½ºÅ×ÀÌÁö ·¹º§¿¡ µû¸¥ ¸Ê »ý¼º
+        Instantiate(_paddle1);
+        Instantiate(_ball);
+
+        Instantiate(_brick);
+
+        // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsClear())
+        {
+            Debug.Log("Game Clear");
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    bool IsClear()
+    {
+        GameObject[] remain_bricks = GameObject.FindGameObjectsWithTag("brick");
+        return remain_bricks.Length == 0;
     }
 
     public void GameOver()
     {
-        GameOverPanel.SetActive(true);
+        //GameOverPanel.SetActive(true);
         Time.timeScale = 0.0f;
     }
 
-    public void Score()
+    public void AddScore(int score)
     {
+        this._score += score;
+        Score.text = _score.ToString();
+        IsClear();
+    }
 
+    public void LostLife()
+    {
+        _life--;
+        Life.text = _life.ToString();
+
+        if (_life < 1)
+        {
+            GameOver();
+        }
+        else
+        {
+            Instantiate(_ball);
+        }
     }
 }
