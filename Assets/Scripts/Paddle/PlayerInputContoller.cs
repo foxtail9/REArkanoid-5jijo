4726c248ae6c8dcd8286ee827de145b130c57618
ojs0124 <ojs_0124_@naver.com>
2024-10-16 23:47:03 +0900

    commit

--- Assets/Scripts/Paddle/PlayerInputContoller.cs
+++ Assets/Scripts/Paddle/PlayerInputContoller.cs
@@ -1,15 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputContoller : PlayerEventManager
 {
-    Vector2 moveInput;
-
     public void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
+        Vector2 moveInput = value.Get<Vector2>();
 
         CallMoveEvent(moveInput);
     }
