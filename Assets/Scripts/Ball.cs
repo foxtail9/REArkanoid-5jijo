f6904ff42c6286b2525dd95282eed866ff75be09
ekrxjvpvj0110 <ekrxjvpvj0110@gmail.com>
2024-10-16 10:50:06 +0900

    feat: add ball reflect when collision wall

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D myRigid;
-    private Vector2 direction;
+
+    private Vector2 ballDirection;
     private float speed = 5f;
+
+    private void Awake()
+    {
+        myRigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        ballDirection = Vector2.up.normalized;
+    }
 
     private void Update()
     {
-        //transform.position += dire
+        transform.position += new Vector3(ballDirection.x, ballDirection.y, 0) * speed * Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("brick"))
         {
             BrickControl brickControl = collision.gameObject.GetComponent<BrickControl>();
 
@@ -23,6 +34,15 @@ public class Ball : MonoBehaviour
             {
                 brickControl.DestroyBrick();
             }
+        }
+
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            ballDirection = Vector2.Reflect(ballDirection, collision.contacts[0].normal);
+        }
+        else
+        {
+
         }
     }
 }
