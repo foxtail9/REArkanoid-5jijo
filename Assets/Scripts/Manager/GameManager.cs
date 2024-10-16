ecbd9b30ae4e5dd0b13f599774c7f51f06430952
ojs0124 <ojs_0124_@naver.com>
2024-10-16 23:54:10 +0900

    Update GameManager.cs

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,38 +9,76 @@ public class GameManager : MonoBehaviour
     {
         if(Instance == null)
         {
-            Instance = new GameManager();
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
         Time.timeScale = 1.0f;
     }
 
-    [SerializeField] private GameObject _paddle;
+    [SerializeField] private GameObject _paddle1;
+    [SerializeField] private GameObject _paddle2;
     [SerializeField] private GameObject _brick;
     [SerializeField] private GameObject _ball;
 
-    [SerializeField] private GameObject GameOverPanel;
+    [SerializeField] private Text Life;
+    [SerializeField] private Text Score;
+    //[SerializeField] private GameObject GameOverPanel;
+
+    private int _life = 3;
+    private int _score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        Instantiate(_paddle1);
+        Instantiate(_ball);
+
         // 스테이지 레벨에 따른 맵 생성
+        Instantiate(_brick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsClear())
+        {
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    bool IsClear()
+    {
+        GameObject[] remain_bricks = GameObject.FindGameObjectsWithTag("brick");
+        return remain_bricks.Length == 0;
     }
 
     public void GameOver()
     {
-        GameOverPanel.SetActive(true);
+        //GameOverPanel.SetActive(true);
         Time.timeScale = 0.0f;
     }
 
-    public void Score()
+    public void AddScore(int score)
     {
-
+        this._score += score;
+        Score.text = _score.ToString();
+        IsClear();
     }
+
+    public void LostLife()
+    {
+        _life--;
+        Life.text = _life.ToString();
+
+        if (_life < 1)
+        {
+            GameOver();
+        }
+        else
+        {
+            Instantiate(_ball);
+        }
+    }
+
 }
