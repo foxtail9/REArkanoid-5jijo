068bb90b3ccb904f8e77df1955f6c60d6ca2d834
keubung <skdud3390@naver.com>
2024-10-17 17:53:31 +0900

    [Add] StagePanel slide effect
    각 스테이지 버튼을 누르면 그에 맞는 스테이지 패널이 활성화 되고 슬라이드 효과로 화면 전환

---
+++ Assets/Scripts/UIController/TestNYUIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TestNYUIManager : MonoBehaviour
+{
+    public RectTransform stagePanel;
+    public RectTransform stageMainPanel;
+
+    void Start()
+    {
+        stagePanel.DOAnchorPos(Vector2.zero, 0.25f);
+    }
+
+    public void StageButton()
+    {
+        stageMainPanel.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        stagePanel.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void BackButton()
+    {
+        stageMainPanel.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        stagePanel.DOAnchorPos(new Vector2(1080, 0), 0.25f);
+    }
+}
