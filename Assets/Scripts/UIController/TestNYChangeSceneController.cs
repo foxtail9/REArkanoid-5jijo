3ea640a0301e94ed73d7e40d572573573777a76c
keubung <skdud3390@naver.com>
2024-10-18 14:12:49 +0900

    [Update] 주석 처리 및 게임씬 텍스트 수정

--- Assets/Scripts/UIController/TestNYChangeSceneController.cs
+++ Assets/Scripts/UIController/TestNYChangeSceneController.cs
@@ -18,3 +18,6 @@ public class TestNYChangeSceneController : MonoBehaviour
         SceneManager.LoadScene("TestNYMain");
     }
 }
+// title(intro) 씬에 바로 스테이지 선택이 아닌 게임종료 버튼과 스테이지 선택, 설정 버튼이 있는 화면에서 스테이지 버튼을 누르면 스테이지 선택 패널이 활성화
+// 메뉴 추가
+// 게임 종료 추가
