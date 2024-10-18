813e438ef96dd53ac364079a75b2c67be8a2d774
ojs0124 <ojs_0124_@naver.com>
2024-10-18 10:21:15 +0900

    commit
    paddle velocity

--- Assets/Scripts/Paddle/PlayerMoveMent.cs
+++ Assets/Scripts/Paddle/PlayerMoveMent.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerInputContoller playerInputContoller;
+    private Rigidbody2D _rigidbody;
+
     private Vector2 movementDirection = Vector2.zero;
     private float _speed = 5f;
 
     private void Awake()
     {
         playerInputContoller = GetComponent<PlayerInputContoller>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -28,6 +31,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void ApplyMovement(Vector2 direction)
     {
-        transform.position += new Vector3(direction.x, 0, 0) * _speed * Time.deltaTime;
+        Vector2 velocity = new Vector2(direction.x * _speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = velocity;
+
+        //transform.position += new Vector3(direction.x, 0, 0) * _speed * Time.deltaTime;
     }
 }
